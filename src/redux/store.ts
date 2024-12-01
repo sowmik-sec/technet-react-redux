8abe920c2c61f106dc8f6a51e8fc267fe30a7127
sowmik-sec <sowmik.sec@gmail.com>
2024-12-01 11:27:03 +0600

    Cart slice and handling add product

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
