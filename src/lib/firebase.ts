bc3599d66de9a6cf1941a24a653f390b35f6011b
sowmik-sec <sowmik.sec@gmail.com>
2024-12-01 20:10:35 +0600

    Handling-promise-stages-with-builder-and-addCase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,21 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.apiKey,
+//   authDomain: import.meta.env.authDomain,
+//   projectId: import.meta.env.projectId,
+//   storageBucket: import.meta.env.storageBucket,
+//   messagingSenderId: import.meta.env.messagingSenderId,
+//   appId: import.meta.env.appId,
+// };
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  apiKey: 'AIzaSyAjhX8zOK7WKG7ZhGPWVMHxYiie4qer0cw',
+  authDomain: 'technet-1303b.firebaseapp.com',
+  projectId: 'technet-1303b',
+  storageBucket: 'technet-1303b.firebasestorage.app',
+  messagingSenderId: '213965843934',
+  appId: '1:213965843934:web:d5f6cd274dfcb55b82814f',
 };
 
 const app = initializeApp(firebaseConfig);
--- src/redux/features/user/userSlice.ts
+++ src/redux/features/user/userSlice.ts
@@ -1,6 +1,5 @@
 import { auth } from '@/lib/firebase';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 interface IUserState {
@@ -42,9 +41,19 @@ const userSlice = createSlice({
     builder
       .addCase(createUser.pending, (state) => {
         state.isLoading = true;
+        state.isError = false;
+        state.error = null;
       })
-      .addCase(createUser.fulfilled, () => {})
-      .addCase(createUser.rejected, () => {});
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.user.email = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.user.email = null;
+        state.isError = true;
+        state.isLoading = false;
+        state.error = action.error.message!;
+      });
   },
 });
 
