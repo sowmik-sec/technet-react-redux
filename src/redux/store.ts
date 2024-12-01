a6945966bd8b37c27486b4fc2892c73c0b78c243
sowmik-sec <sowmik.sec@gmail.com>
2024-12-01 12:18:36 +0600

    Product filters using redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
+import productReducer from './features/products/productSlice';
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    product: productReducer,
   },
 });
 
